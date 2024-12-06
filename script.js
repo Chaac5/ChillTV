0ead5258fd4b19887421c83be6d1097007366c0c
Denver <krortegaram0@gmail.com>
2024-12-06 17:06:50 -0600

    se agrego la primera idea de la caratula de la movie

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let categoryMovies = {
+    action: [
+        { 
+            id: 1, 
+            title: 'Hijos de Perra', 
+            year: 20023, 
+            director: 'Josh Greenbaum',
+            duration: 93,
+            cover: 'https://www.covercaratulas.com/ode/mini/carteles-49881.jpg'
+        }
+    ]
+}
